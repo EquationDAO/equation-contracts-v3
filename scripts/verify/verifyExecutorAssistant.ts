ad1b29d5a8b32ddffc8740583e1e69752dc9992a
Christopher-Michael-Pratt <133328142+Christopher-Michael-Pratt@users.noreply.github.com>
2024-05-12 21:12:41 +0800

    add verify scripts (#8)

--- scripts/verify/verifyExecutorAssistant.ts
+++ scripts/verify/verifyExecutorAssistant.ts
@@ -2,6 +2,4 @@ import "dotenv/config";
 
 const document = require(`../../deployments/${process.env.CHAIN_ID}.json`);
 
-module.exports = [
-    `${document.deployments.PositionRouter}`,
-];
+module.exports = [`${document.deployments.PositionRouterUpgradeable}`];
--- scripts/verify/verifyPriceFeed.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import {networks} from "../networks";
-import {hardhatArguments} from "hardhat";
-
-module.exports = [
-    networks[hardhatArguments.network as keyof typeof networks].usdChainLinkPriceFeed,
-    0,
-    networks[hardhatArguments.network as keyof typeof networks].ignoreReferencePriceFeedError,
-];
